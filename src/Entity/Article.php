7621fd1b65ac4277a07780f9bc99c1a4d495b55c
maram laakom <maramlaakom2@gmail.com>
2026-02-28 22:41:25 +0100

    feat: Entité Article avec Doctrine, migration et CRUD basique

---
+++ src/Entity/Article.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ArticleRepository::class)]
+class Article
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type:"integer")]
+    private ?int $id = null;
+
+    #[ORM\Column(type:"string", length:255)]
+    private ?string $titre = null;
+
+    #[ORM\Column(type:"text")]
+    private ?string $contenu = null;
+
+    #[ORM\Column(type:"string", length:255)]
+    private ?string $auteur = null;
+
+    #[ORM\Column(type:"datetime")]
+    private ?\DateTimeInterface $dateCreation = null;
+
+    #[ORM\Column(type:"boolean")]
+    private ?bool $publie = null;
+
+    // ===== GETTERS & SETTERS =====
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+        return $this;
+    }
+
+    public function getContenu(): ?string
+    {
+        return $this->contenu;
+    }
+
+    public function setContenu(string $contenu): self
+    {
+        $this->contenu = $contenu;
+        return $this;
+    }
+
+    public function getAuteur(): ?string
+    {
+        return $this->auteur;
+    }
+
+    public function setAuteur(string $auteur): self
+    {
+        $this->auteur = $auteur;
+        return $this;
+    }
+
+    public function getDateCreation(): ?\DateTimeInterface
+    {
+        return $this->dateCreation;
+    }
+
+    public function setDateCreation(\DateTimeInterface $dateCreation): self
+    {
+        $this->dateCreation = $dateCreation;
+        return $this;
+    }
+
+    public function getPublie(): ?bool
+    {
+        return $this->publie;
+    }
+
+    public function setPublie(bool $publie): self
+    {
+        $this->publie = $publie;
+        return $this;
+    }
+}
